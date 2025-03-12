e06bbb6204defdc6b716056ce679bd4b3c67a069
mash <urexz123@gmail.com>
2025-03-12 18:36:08 +0700

    PostForm.jsx on 11blog app done

--- 11blogapp/src/App.jsx
+++ 11blogapp/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         </main>
       </div>
       <h1 className="text-3xl font-bold text-center mt-5 bg-slate-900 text-white p-5 rounded-md border-solid border-4 border-blue-600 hover:bg-green-700 transition-all duration-300 ease-in-out cursor-pointer" >Youtube Blog</h1>
-      <Footer />  // Adding Footer component to render the footer
+      <Footer />
     </>
   )
 }
